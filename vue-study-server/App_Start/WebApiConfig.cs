0ebb68583e20f0762aa425c914dda6912397fe8b
yzx <494132721@qq.com>
2017-04-25 17:17:23 +0800

    vue Server初始

---
+++ vue-study-server/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace vue_study_server.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ vue-study-server/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vue_study_server.Global" Language="C#" %>
